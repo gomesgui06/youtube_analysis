b2519ff54a028d42eb59b54a9c456ac3c1d03844
Guilherme Gomes <gg.gomesguilherme@gmail.com>
2020-10-19 10:09:43 -0300

    Testando pegar os comentários de uma lista de vídeos

--- main.py
+++ main.py
@@ -33,43 +33,53 @@ def get_url_video(url_channel):
     return (df)
 
 def get_video_comment(video):
-    coments=[]
+    comments=[]
     # votes =[]
-    authors = []
+    # authors = []
 
     with Chrome('/home/guilherme.gomes/chromedriver') as driver:
         wait = WebDriverWait(driver,50)
         driver.get(video)
 
-        for item in range(10): 
+        for item in range(5): 
             wait.until(EC.visibility_of_element_located((By.TAG_NAME, "body"))).send_keys(Keys.END)
-            print(EC.visibility_of_element_located((By.TAG_NAME, "body")))
-            print(wait.until(EC.visibility_of_element_located((By.TAG_NAME, "body"))).send_keys(Keys.END))
             time.sleep(15)
 
         for comment in wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "#content-text"))):
-            coments.append(comment.text)
+            comments.append(comment.text)
+            print(comment.text)
         """
         for vote in wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "#vote-count-middle"))):
             votes.append(vote.text)
-        """
+        
         for author in wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "#author-text"))):
             authors.append(author.text)
-    
-        df = pd.DataFrame(coments, columns=['comment'])
-        df['author'] = authors
+        """
+        df = pd.DataFrame(comments, columns=['comment'])
+        # df['comment'] = coments
+        # df['vote'] = votes
 
     return df
 
 def main():
-    # To Do
-    # Passar a lista de vídeos e iterar pegando os comentários
-    url_channel = 'https://www.youtube.com/user/MrMatheusTor/videos'
+    #url_channel = 'https://www.youtube.com/c/LionBBQ/videos'
+    # url_channel = 'https://www.youtube.com/user/MrMatheusTor/videos'
     # video = 'https://www.youtube.com/watch?v=Qw_cU8fN8aU&ab_channel=MrMatheusTor'
     
-    df = get_url_video(url_channel)
-    # df = get_video_comment(video=video)
-    df.to_csv('teste.csv')
+    #df_youtube_videos = get_url_video(url_channel)
+    df_youtube_videos = pd.read_csv('datalake/raw/youtube_videos.csv')
+    youtube_video_list = df_youtube_videos.url.tolist()
+    name_video_list = df_youtube_videos.video_title.tolist()
+
+    count = 0 
+    for youtube_video in youtube_video_list:
+        
+        print(f'vídeo: {youtube_video}')
+        df_coments = get_video_comment(video=youtube_video)
+        df_coments.to_csv(f'datalake/raw/coments_of_{name_video_list[count]}.csv')
+        count = count+1    
+    
+    #df_youtube_videos.to_csv('datalake/raw/youtube_videos.csv')
 
 
 if __name__ == '__main__':

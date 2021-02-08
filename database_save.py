bd75fa8f7ea25d2c8e5177e4d70d35a251e0a339
gomesgui06 <gg.gomesguilherme@gmail.com>
2021-02-08 18:28:34 -0300

    Adicionando docker-compose do projeto com postgres e metabase

--- database_save.py
+++ database_save.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
 
-def save_postgres(df, con, save_postgres):
+def save_postgres(df, con, folder, nome_tabela):
     engine = create_engine(f'{con}/{folder}')
     if not database_exists(engine.url):
         create_database(engine.url)
 
     print(database_exists(engine.url))
-    df.to_sql(folder, engine)
+    df.to_sql(f'{folder}_{nome_tabela}', engine, if_exists='replace')
